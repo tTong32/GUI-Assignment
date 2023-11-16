d9fb4a23061f2832cc724631d1b1974ea7fa65d3
hihi420 <151059054+hihi420@users.noreply.github.com>
2023-11-16 12:38:25 -0500

    Update untitled.java

--- untitled.java
+++ untitled.java
@@ -5,11 +5,14 @@ import javax.swing.event.*;
 
 public class untitled{
 	// Properties
-	
+	JFrame theframe = new JFrame("GUI Project");
+	JPanel thePanel = new JPanel();
 	// Methods
 	
 	// Constructor
-	
+	public untitled(){
+		thepanel.setPrefferedSize(new Dimension(1000, 1000);
+	}
 	// Main Method
 	public static void main(String[] args){
 		//Dljsladfkjlafjlk
