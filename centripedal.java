f760ccb2d35d05f41dca2762c18cfce25158b079
hihi420 <151059054+hihi420@users.noreply.github.com>
2023-11-21 13:10:12 -0500

    Update to centripedal.java

--- centripedal.java
+++ centripedal.java
@@ -11,6 +11,15 @@ public class centripedal implements ActionListener, KeyListener, MouseListener, 
 	JFrame theFrame = new JFrame("GUI Project");
 	newpanel thePanel = new newpanel();
 	Timer theTimer = new Timer(1000/48, this);
+	JLabel massLabel = new JLabel("Mass");
+	JTextField massText = new JTextField();
+	JLabel velocityLabel = new JLabel("Velocity");
+	JTextField velocityText = new JTextField();
+	JLabel radiusLabel = new JLabel("Radius");
+	JTextField radiusText = new JTextField();
+	JLabel periodLabel = new JLabel("Period");
+	JTextField periodText = new JTextField();
+	JButton pictureButton = new JButton("Form Picture");
 	
 	// Methods
 	public void actionPerformed(ActionEvent evt){
@@ -41,7 +50,44 @@ public class centripedal implements ActionListener, KeyListener, MouseListener, 
 	
 	// Constructor
 	public centripedal(){
-		thePanel.setPreferredSize(new Dimension(1000, 1000));
+		thePanel.setLayout(null);
+		thePanel.setPreferredSize(new Dimension(960, 540));
+		
+		
+		massText.setSize(100, 30);
+		massText.setLocation(100,20);
+		thePanel.add(massText);
+		massLabel.setSize(75, 65);
+		massLabel.setLocation(30, 0);
+		thePanel.add(massLabel);
+		
+		velocityText.setSize(100, 30);
+		velocityText.setLocation(100,70);
+		thePanel.add(velocityText);
+		velocityLabel.setSize(75, 115);
+		velocityLabel.setLocation(30, 30);
+		thePanel.add(velocityLabel);
+		
+		radiusText.setSize(100, 30);
+		radiusText.setLocation(100,120);
+		thePanel.add(radiusText);
+		radiusLabel.setSize(75, 165);
+		radiusLabel.setLocation(30, 50);
+		radiusText.setSize(100, 30);
+		thePanel.add(radiusLabel);
+		
+		periodText.setSize(100, 30);
+		periodText.setLocation(100, 170);
+		thePanel.add(periodText);
+		periodLabel.setSize(75, 215);
+		periodLabel.setLocation(30, 80);
+		thePanel.add(periodLabel);
+		
+		pictureButton.setSize(190, 40);
+		pictureButton.setLocation(25, 225);
+		pictureButton.addActionListener(this);
+		thePanel.add(pictureButton);
+		
 		theFrame.setContentPane(thePanel);
 		theFrame.pack();
 		theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
