42df66526c707fc466a376df40c0765624d9acb7
Thom12039 <tomtong2006@gmail.com>
2023-11-22 13:21:55 -0500

    Update newpanel.java

--- newpanel.java
+++ newpanel.java
@@ -6,9 +6,54 @@ import javax.imageio.*;
 
 public class newpanel extends JPanel{
 	//Properties
+	int intPosX = 740;
+	int intPosY = 220;
+	int intVeloX = 0;
+	int intVeloY = 10;
+	int intAccel;
+	int intAccelX = 2;
+	int intAccelY = -2;
+	int accelChange = 2;
 	
+	int intChangeX = 690;
+	int intChangeY = 270;
+	
+	double dblMass;
+	double dblVelocity;
+	double dblPeriod;
+	double dblRadius;
 	//Methods
-	
+	public void paintComponent(Graphics g){
+		
+		g.setColor(Color.WHITE);
+		g.fillRect(0,0,960,540);
+		
+		g.setColor(Color.RED);
+		g.fillOval(intPosX, intPosY, 20, 20);
+		
+		if(intPosX < intChangeX){
+			intAccelX = accelChange;
+		}
+		if(intPosX >= intChangeX){
+			intAccelX = -accelChange;
+		}
+		if (intPosY <= intChangeY){
+			intAccelY = accelChange;
+			System.out.println("Going down");
+		}
+		if (intPosY >= intChangeY){
+			intAccelY = -accelChange;
+			System.out.println("Going up");
+		}
+		
+		intVeloX += intAccelX;
+		intVeloY += intAccelY;
+		intPosX += intVeloX;
+		//intPosY += intVeloY;
+		
+		System.out.println(intVeloX);
+		System.out.println(intVeloY);
+	}
 	//Constructor
 }
 
