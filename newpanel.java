8e9082bf612ee7d01d1ffe7f70b6832ec20b283c
hihi420 <151059054+hihi420@users.noreply.github.com>
2023-12-06 09:53:21 -0500

    Revert "aded comments, removed arrow indicator"
    This reverts commit e66aa61978d3ee08439522a2b844dfcdb2042b2d.

--- newpanel.java
+++ newpanel.java
@@ -11,74 +11,80 @@ public class newpanel extends JPanel{
 	
 	//Properties
 
-	// x and y position of ball
 	int intPosX = 740;
-	int intPosY = 270;
+	int intVeloX;
+	int intAccelX;
 	
-	// how many degrees the ball has rotated off the horizontal
+	int intPosY = 270;
+	int intVeloY;
+	int intAccelY;
+	
+	boolean blnGravity = true;
+	
 	double dblDegrees = 0;
 	
-	// variables that determine the overall projection
 	double dblMass = 300; // in kg
 	double dblPeriod = 8; // seconds it takes for a revolution
 	double dblRadius = 25; // every 5 pixels is 1 m
 	double dblForceCentr; // in pi N
 
-	// Setting up the force indicator
 	Rectangle forceRect = new Rectangle(690, 265, 0, 10);
-	// Affine transform is used such that only the rectangle can rotate according to an anchor point
-	// Using g2d.rotate also rotates other JComponents and thus does not work
+	Polygon arrowShape = new Polygon();  
+	Point arrowHeadPoint = new Point(690, 270);
+	Point arrowTopPoint = new Point(690, 265);
+	Point arrowBotPoint = new Point(690, 275);
+	arrowShape.addPoint(arrowHeadPoint);
+	arrowShape.addPoint(arrowTopPoint);
+	arrowShape.addPoint(arrowBotPoint);
+
 	AffineTransform t = new AffineTransform();
 	Shape rForceRect = t.createTransformedShape(forceRect);
+	Shape rArrowShape = t.createTransformedShape(arrowShape);
+
+
+	int arrowWidth;
+	int arrowHeight;
 	
-	// Setting up mass and radius as an integer in order for easier graphical conversion
 	int intMass = (int)dblMass / 10;
 	int intRadius = (int)dblRadius * 5;
 
-	// The "tracer", or the projection of the overall movement of the "mass"
 	int intTracerDiameter = intRadius*2 + intMass/4;
 	int intTracerRadius = intRadius + intMass/8;
 	int intTracerWidth = intMass / 4;
 
-	// set a new color for left side of screen
 	Color greyColor = new Color(247, 242, 242);
 
-	// Default color settings for the force rectangle
 	int intForceR = 255;
 	int intForceG = 180;
 	int intForceB = 180;
 	Color forceColor = new Color(intForceR, intForceG, intForceB);
 
+	JLabel fcLabel = new JLabel();
+
 	//Methods
 	public void paintComponent(Graphics g){
 		
-		// Every repaint(), update the mass and radius, even if the user has not changed it
 		intMass = (int)dblMass / 10;
 		intRadius = (int)dblRadius * 5;
 
-		// The tracer is used to "map" out the movement of the ball - update every repaint()
+		// The tracer is used to "map" out the movement of the ball
 		intTracerDiameter = intRadius*2 + intMass/4;
 		intTracerRadius = intRadius + intMass/8;
 		intTracerWidth = intMass / 4;
 
-		// Since centripetal force is divided by period, a math error occurs if period = 0
 		if (dblPeriod == 0){
 			dblForceCentr = 0;
 		} else {
-			// otherwise, the use the derived equation for centripetal force using mass, radius and period
 			dblForceCentr = (4*dblMass*dblRadius) / (dblPeriod*dblPeriod);
 		}
 		
-		// Graphics 2D is required in order to rotate the centripetal foce rectangle
 		Graphics2D g2d = (Graphics2D)g;
 
-		// set the force color back to default settings
 		intForceR = 255;
 		intForceB = 180;
 		intForceG = 180;
 
-		// and if else if chain setting the size and color of the force rectangle indicator
-		if (dblForceCentr > 0 && dblForceCentr < 200 && dblPeriod != 0){
+		if (dblForceCentr > 0 && dblForceCentr < 100 && dblPeriod != 0){
 			forceRect.height = 1;
 			forceRect.width = intTracerRadius;
 			forceRect.y = 270;
@@ -98,12 +104,17 @@ public class newpanel extends JPanel{
 			}
 			forceRect.width = intTracerRadius;
 			forceRect.height = 50;
-			forceRect.y = (245);
 		} else {
 			forceRect.width = intTracerRadius;
 			forceRect.height = (int)dblForceCentr/200;
 			forceRect.y = (int)(270 - dblForceCentr/400);
 		}
+
+		arrowHeight = forceRect.height*3;
+		arrowWidth = forceRect.width/5;
+		arrowShape.addPoint(690 - arrowWidth + intTracerRadius/2,270);
+		arrowShape.addPoint(690 + arrowWidth + intTracerRadius/2,270 - arrowHeight);
+		arrowShape.addPoint(690 + arrowWidth +  intTracerRadius/2,270 + arrowHeight);
 
 		g.setColor(greyColor);
 		g.fillRect(0, 0, 400, 540);
@@ -119,6 +130,7 @@ public class newpanel extends JPanel{
 
 		g.setColor(forceColor);
 		g2d.fill(rForceRect);
+		g2d.fill(arrowShape);
 
 		g.setColor(Color.RED);
 		g.fillOval(intPosX - (intMass / 2), intPosY - (intMass / 2), intMass, intMass);
@@ -130,6 +142,8 @@ public class newpanel extends JPanel{
 			t.translate(690, 270);
 			t.rotate(Math.toRadians(360/(48*dblPeriod)));
 			t.translate(-690, -270);
+
+			rArrowShape = t.createTransformedShape(arrowShape);
 			rForceRect = t.createTransformedShape(forceRect);
 		} 
 		
